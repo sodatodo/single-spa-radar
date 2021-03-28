dd2c341919b66c174e114d7b3e015b20eb911f07
WangSoda <wangsuda@icloud.com>
2021-03-28 22:03:17 +0800

    feat(): radar ok

--- src/core.ts
+++ src/core.ts
@@ -1,12 +1,24 @@
 import { Application, ExtraProps } from 'single-spa/typings/single-spa'
 import { promiseForEach } from './promiseLoop'
 
-const singleSpaRadar = (applicationOrLoadingFns: Application<ExtraProps>[]) => {
-    return new Promise((resolve, reject) => {
-       promiseForEach(applicationOrLoadingFns, (application) => {
-        resolve(application);
-       });
-    });
+const singleSpaRadar = (applicationOrLoadingFns: Application<ExtraProps>[], releaseLoadingFn: Application<ExtraProps>, isRelease: boolean = false) => {
+    if (isRelease) {
+        return new Promise((resolve) => {
+            releaseLoadingFn().then(application => {
+                resolve(application);
+            });
+        })
+    } else {
+        if (releaseLoadingFn) {
+            applicationOrLoadingFns.push(releaseLoadingFn);
+        }
+        return new Promise((resolve, reject) => {
+            promiseForEach(applicationOrLoadingFns, (application) => {
+             resolve(application);
+            });
+         });
+    }
+    
 }
 
 export {
